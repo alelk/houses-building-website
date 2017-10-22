5ffdeeeccb3017346223778848469eef212c52d4
Alex Elkin <elckinne@gmail.com>
2017-10-22 14:38:34 +0300

    Working on header & menu functionality

--- src/header/AboutCompanyPanel.js
+++ src/header/AboutCompanyPanel.js
@@ -4,18 +4,17 @@
  * Created by Alex Elkin on 19.10.2017.
  */
 
-import {Contacts} from '../data/data'
 import Panel from '../panel/Panel'
 import './AboutCompanyPanel.css'
-import logo from './logo.svg'
+import '../img/sprite-48px-white.css'
 
 import React from 'react';
 import PropTypes from 'prop-types'
 
-const AboutCompanyPanel = ({className}) => {
+const AboutCompanyPanel = ({className, onLogoClick}) => {
     return (
         <Panel className={`AboutCompanyPanel${(className && ' ' + className) || ''}`}>
-            <img className="logo" src={logo}/>
+            <div onClick={onLogoClick} className="logo ic_store_white_48px"/>
             <label className="company">
                 <span className="name">Строительная бригада</span>
                 <span className="location">г. Павлово</span>
@@ -24,6 +23,7 @@ const AboutCompanyPanel = ({className}) => {
     )
 };
 AboutCompanyPanel.propTypes = {
-    className: PropTypes.string
+    className: PropTypes.string,
+    onLogoClick: PropTypes.func
 };
 export default AboutCompanyPanel;
